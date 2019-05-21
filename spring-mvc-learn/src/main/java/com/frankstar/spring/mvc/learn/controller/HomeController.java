85bf6344498e1723ff207a6dbaf57e77b2cc291e
frankstar <yehongxing@meituan.com>
2019-05-21 20:06:24 +0800

    fix data

--- spring-mvc-learn/src/main/java/com/frankstar/spring/mvc/learn/controller/HomeController.java
+++ spring-mvc-learn/src/main/java/com/frankstar/spring/mvc/learn/controller/HomeController.java
@@ -1,5 +1,11 @@
 package com.frankstar.spring.mvc.learn.controller;
 
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.mvc.Controller;
+
 /**
  * @Author :  frankstar
  * @AddTime :  2019/5/21
@@ -7,6 +13,18 @@ package com.frankstar.spring.mvc.learn.controller;
  * @Project :  java-improve
  * @Desc :
  */
-public class HomeController {
+@RequestMapping
+public class HomeController implements Controller {
 
+
+	@Override
+	public ModelAndView handleRequest(HttpServletRequest httpServletRequest,
+		HttpServletResponse httpServletResponse) throws Exception {
+		//返回ModelAndView
+		ModelAndView modelAndView = new ModelAndView();
+		//指定视图
+		modelAndView.setViewName("/WEB-INF/view/hello.jsp");
+
+		return modelAndView;
+	}
 }
