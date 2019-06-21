68f3c7ea9ecf1ab5a5d112d41f72d3eb63db1753
frankstar <yehongxing@meituan.com>
2019-06-21 18:15:26 +0800

    add spring boot

---
+++ spring-boot-learn/src/main/java/com/frankstar/springboot/permission/dao/RoleDao.java
@@ -0,0 +1,12 @@
+package com.frankstar.springboot.permission.dao;
+
+/**
+ * @Author :  frankstar
+ * @AddTime :  2019/6/21
+ * @EMail :  yehongxing@meituan.com
+ * @Project :  java-improve
+ * @Desc :
+ */
+public interface RoleDao {
+
+}
---
+++ spring-boot-learn/src/main/java/com/frankstar/springboot/permission/enity/UserRoleEntity.java
@@ -0,0 +1,27 @@
+package com.frankstar.springboot.permission.enity;
+
+import java.io.Serializable;
+import java.util.Date;
+import lombok.Data;
+
+/**
+ * @Author :  frankstar
+ * @AddTime :  2019/6/21
+ * @EMail :  yehongxing@meituan.com
+ * @Project :  java-improve
+ * @Desc :
+ */
+@Data
+public class UserRoleEntity implements Serializable{
+
+	private int id;
+
+	private int userId;
+
+	private int roleId;
+
+	private Date addTime;
+
+	private Date updateTime;
+
+}
