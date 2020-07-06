b308680bebb17bf2af3d31123cf21cade298560b
yons <yons@iMac-2.local>
2020-07-07 00:22:28 +0800

    fix squirrel nuts

--- java-algorithm-learn/src/main/java/com/frankstar/leetcode/数组/_0706_573松鼠模拟.java
+++ java-algorithm-learn/src/main/java/com/frankstar/leetcode/数组/_0706_573松鼠模拟.java
@@ -22,30 +22,43 @@ public class _0706_573松鼠模拟 {
 
         //1 先计算每个坚果到树的距离
         int nutsN = nuts.length;
-        Map<Integer, Integer> distMap = new HashMap<>();
+        int sum = 0;
         for (int i=0; i<nutsN; i++) {
             int dist = computeDist(tree[0], tree[1], nuts[i][0], nuts[i][1]);
-            distMap.put(i, dist);
+            sum += dist * 2;
         }
         int min = Integer.MAX_VALUE;
-        Map<Integer, Integer> minMap = new HashMap<>();
+       // 2计算每个nut 与松鼠的距离
         for (int i=0; i<nutsN; i++) {
             int dist = computeDist(squirrel[0], squirrel[1], nuts[i][0], nuts[i][1]);
-            minMap.put(dist, i);
-            min = Math.min(min, dist);
+            int cur = sum - computeDist(tree[0], tree[1], nuts[i][0], nuts[i][1])
+            + dist;
+            min = Math.min(min, cur);
         }
-        int sum = min + distMap.get(minMap.get(min));
-        for (Integer index : distMap.keySet()) {
-            if (!minMap.get(min).equals(index)) {
-                sum += 2 * distMap.get(index);
-            }
-        }
-        return sum;
+        return min;
     }
 
     private int computeDist(int treeLeft, int treeRight, int nutLeft, int nutRight) {
         return Math.abs(treeLeft - nutLeft) + Math.abs(treeRight - nutRight);
     }
+
+
+    public int getDistance(int[] a, int[] b) {
+        return Math.abs(a[0] - b[0]) + Math.abs(a[1] - b[1]);
+    }
+
+    public int minDistanceV2(int height, int width, int[] tree, int[] squirrel, int[][] nuts) {
+        int ans = (int) 1e9;
+        int sum_dis = 0;
+        for (int[] nut : nuts)
+            sum_dis += getDistance(nut, tree) * 2;
+        for (int[] first_nut : nuts) {
+            int cur = sum_dis - getDistance(first_nut, tree) + getDistance(first_nut, squirrel);
+            ans = Math.min(cur, ans);
+        }
+        return ans;
+    }
+
 
 
     public static void main(String[] args) {
